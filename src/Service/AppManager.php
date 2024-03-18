b3a6be00bdfee4deccc144b6348b444675791039
tricoire arnaud <arnaud.tricoire@hotmail.com>
2024-03-18 15:45:51 +0100

    création AppManager

---
+++ src/Service/AppManager.php
@@ -0,0 +1,91 @@
+<?php 
+// src/Service/AppManager.php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
+
+class AppManager
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        
+        $this->entityManager = $entityManager;
+    }
+
+    public function registerUser(array $userData): User
+    {
+        $user = new User();
+        $user->setName($userData['name']);
+        $user->setEmail($userData['email']);
+        $hashedPassword = password_hash($userData['password'], PASSWORD_DEFAULT);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+
+    public function updateUser(User $user, array $userData): User
+    {
+        $user->setName($userData['name']);
+        $user->setEmail($userData['email']);
+        if (isset($userData['password'])) {
+            $hashedPassword = password_hash($userData['password'], PASSWORD_DEFAULT);
+            $user->setPassword($hashedPassword);
+        }
+
+        $this->entityManager->flush();
+
+        return $user;
+    }
+
+    public function deleteUser(User $user): void
+    {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+    }
+
+    public function createEvent(array $eventData, User $creator): Event
+    {
+        $event = new Event();
+        $event->setTitle($eventData['title']);
+        $event->setDescription($eventData['description']);
+        $event->setBeginDate(new \DateTime($eventData['begin_date']));
+        $event->setEndDate(new \DateTime($eventData['end_date']));
+        $event->setPlace($eventData['place']);
+        $event->setCreator($creator);
+
+        $this->entityManager->persist($event);
+        $this->entityManager->flush();
+
+        return $event;
+    }
+
+    public function updateEvent(Event $event, array $eventData): Event
+    {
+        $event->setTitle($eventData['title']);
+        $event->setDescription($eventData['description']);
+        $event->setBeginDate(new \DateTime($eventData['begin_date']));
+        $event->setEndDate(new \DateTime($eventData['end_date']));
+        $event->setPlace($eventData['place']);
+
+        $this->entityManager->flush();
+
+        return $event;
+    }
+
+    public function deleteEvent(Event $event): void
+    {
+        $this->entityManager->remove($event);
+        $this->entityManager->flush();
+    }
+
+   
+}
